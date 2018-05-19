e627151468eea4f33eb1b880acd9f8fc83f0d1fd
Unknown <samer.elhamdoo@gmail.com>
2018-05-19 04:23:28 +0300

    الدرس الخامس
    التعامل مع shell التحكم بالمنتجات
    اضافة دالة الحفظ لاختيار slug من خلال الاسم

--- coretabs/shop/admin.py
+++ coretabs/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+
+admin.site.register(models.Product) # This will add Product to admin site
+admin.site.register(models.Category) # This will add Category to admin site
 
 # Register your models here.
--- coretabs/shop/models.py
+++ coretabs/shop/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.text import slugify
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -8,6 +10,14 @@ class Category(models.Model):
     description = models.TextField(max_length=200, blank=True)
     slug = models.SlugField(unique=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
     class Meta:
         ordering = ('name',)
 
@@ -21,6 +31,17 @@ class Product(models.Model):
     decription = models.TextField(blank=True)
     slug = models.SlugField(unique=True, blank=True)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
 
     class Meta:
         ordering = ('name',)
